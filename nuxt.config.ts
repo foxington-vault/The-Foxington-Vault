9b3fec745d77af900a62e9410b6c01ca920a60e8
Kerby Keith Aquino <94678583+skepfusky@users.noreply.github.com>
2022-12-18 08:47:02 +0800

    Add Umami tracking code

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,22 +1,28 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-	typescript: {
-		shim: false
-	},
-	css: ['~/assets/css/main.scss'],
-	app: {
-		head: {
-			link: [
-				{ rel: "icon", href: "./favicon.ico" }
-			]
-		}
-	},
-	modules: ['@nuxt/content', '@nuxtjs/color-mode'],
-	plugins: [{ src: '~/plugins/vercel.js', mode: 'client' }],
-	postcss: {
-		plugins: {
-			tailwindcss: {},
-			autoprefixer: {},
-		},
-	},
+  typescript: {
+    shim: false,
+  },
+  css: ["~/assets/css/main.scss"],
+  app: {
+    head: {
+      link: [{ rel: "icon", href: "./favicon.ico" }],
+      script: [
+        {
+          src: "https://umami.kurofusky.xyz/umami.js",
+          async: true,
+          defer: true,
+          "data-website-id": "d1e1b841-cee6-4cd2-a777-477700d4f5a2",
+        },
+      ],
+    },
+  },
+  modules: ["@nuxt/content", "@nuxtjs/color-mode"],
+  plugins: [{ src: "~/plugins/vercel.js", mode: "client" }],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
 })
