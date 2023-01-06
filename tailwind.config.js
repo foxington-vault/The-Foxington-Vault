1629b8b6668a3fcd5ebd10a20d7ed7c43a6d67f6
Kerby Keith Aquino <94678583+skepfusky@users.noreply.github.com>
2023-01-06 20:35:46 +0800

    tailwind plugin tomfoolery

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,41 @@
 /** @type {import("tailwindcss").Config} */
+const plugin = require("tailwindcss/plugin")
 const defaultTheme = require("tailwindcss/defaultTheme")
 
 module.exports = {
-	content: [
-		"./components/**/*.{js,vue,ts}",
-		"./layouts/**/*.vue",
-		"./pages/**/*.vue",
-		"./plugins/**/*.{js,ts}",
-		"./nuxt.config.{js,ts}",
-		"./app.vue",
-	],
-	theme: {
-		extend: {
-			fontFamily: {
-				"lucky-body": ["LuckysTale-Body", ...defaultTheme.fontFamily.sans],
-				"lucky-bold": ["LuckysTale-Bold-New", ...defaultTheme.fontFamily.sans],
-				"inter": ["Inter", ...defaultTheme.fontFamily.sans],
-				"open-sans": ["Open Sans", ...defaultTheme.fontFamily.sans],
-			}
-		},
-	},
-	plugins: [],
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./nuxt.config.{js,ts}",
+    "./app.vue",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        "lucky-body": ["LuckysTale-Body", ...defaultTheme.fontFamily.sans],
+        "lucky-bold": ["LuckysTale-Bold-New", ...defaultTheme.fontFamily.sans],
+        inter: ["Inter", ...defaultTheme.fontFamily.sans],
+        "open-sans": ["Open Sans", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [
+    plugin(({ addBase, addComponents, theme }) => {
+      addBase({
+        html: {
+					scrollBehavior: 'smooth'
+				},
+        body: {
+          fontFamily: theme("fontFamily.open-sans"),
+        },
+      }),
+        addComponents({
+          ".screen-responsive": {
+            marginInline: "auto",
+          },
+        })
+    }),
+  ],
 }
