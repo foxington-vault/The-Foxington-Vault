21899ee8c6fe2fbfed2e72ddfa2f89d450c074d8
Kerby Keith Aquino <94678583+skepfusky@users.noreply.github.com>
2023-01-06 20:22:05 +0800

    Remove Umami script

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -27,14 +27,6 @@ export default defineNuxtConfig({
           href: "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css",
         },
       ],
-      script: [
-        {
-          src: "https://umami.kurofusky.xyz/umami.js",
-          async: true,
-          defer: true,
-          "data-website-id": "d1e1b841-cee6-4cd2-a777-477700d4f5a2",
-        },
-      ],
     },
   },
   modules: ["@nuxt/content", "@nuxtjs/color-mode"],
