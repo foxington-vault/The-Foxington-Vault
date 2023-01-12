225252a9540f094c64f3222a8a5d373f761f0614
Kerby Keith Aquino <94678583+skepfusky@users.noreply.github.com>
2023-01-12 23:58:56 +0800

    add msft clarity

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -27,6 +27,16 @@ export default defineNuxtConfig({
           href: "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css",
         },
       ],
+      script: [
+        {
+          hid: "clarity",
+          children: `(function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "${process.env.CLARITY_ID}");`,
+        },
+      ],
     },
   },
   modules: ["@nuxt/content", "@nuxtjs/color-mode", "@kevinmarrec/nuxt-pwa"],
@@ -38,16 +48,16 @@ export default defineNuxtConfig({
     },
   },
   pwa: {
-		icon: false,
-		meta: {
-			lang: "en",
-			title: "",
-			ogTitle: "",
-			ogSiteName: "The Foxingtion Vault",
-			ogType: "",
-			description: "",
-			twitterCard: "summary",
-		},
+    icon: false,
+    meta: {
+      lang: "en",
+      title: "",
+      ogTitle: "",
+      ogSiteName: "The Foxingtion Vault",
+      ogType: "",
+      description: "",
+      twitterCard: "summary",
+    },
     manifest: {
       name: "The Foxington Vault",
       short_name: "Foxington Vault",
